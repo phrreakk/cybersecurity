4f2f7d0e4b9777d7a322d334c0931ecb9a51d995
Kevin Gangi <kevin_gangi@yahoo.com>
2024-04-02 13:55:08 -0400

    Variable assignment

---
+++ GoogleCert/python/variables.py
@@ -0,0 +1,9 @@
+# Using variables
+# Use a variable to store a device ID
+device_id = "h32rb17"
+print(device_id)
+print("m50pi31")
+data_type = type(device_id)
+print(data_type) # string
+print(type(1.0)) # print out the type of data, aka float
+print(device_id+"test") # Concatenation
