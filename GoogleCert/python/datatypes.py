181b96a86fc452f8f04b3073dca10cb33530edef
Kevin Gangi <kevin_gangi@yahoo.com>
2024-04-02 13:29:04 -0400

    Learning data types

---
+++ GoogleCert/python/datatypes.py
@@ -0,0 +1,10 @@
+# Data types
+# string: text
+# float: 1.2, 10.8234
+# integer: 2, 4, 5, 7
+# boolean: true or false
+# lists: collection of data in sequential form
+print(1+1) # integer
+print(10<5) # false
+print(9<12) # true
+print(["You", "are", "doing", "it!"]) # list
