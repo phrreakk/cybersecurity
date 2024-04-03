1711af323917e65ba80068eb8b2dfcc968a14b9f
Kevin Gangi <kevin_gangi@yahoo.com>
2024-04-02 23:09:43 -0400

    Builtin functions

---
+++ GoogleCert/python/builtin_funcs.py
@@ -0,0 +1,13 @@
+# Let's go over builtin Python functions
+print("Test")
+print(type(4))
+
+a = 3
+b = 9
+c = 6
+print(max(a, b, c))
+
+usernames = ["mgreeberg", "djohnson", "mthomas", "sgutenberg"]
+ids = [33, 21, 3, 9, 2, 90]
+print(sorted(usernames))
+print(sorted(ids, reverse=True))
