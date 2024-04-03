290154f068b9133b90586cc7aac5ef3924d94052
Kevin Gangi <kevin_gangi@yahoo.com>
2024-04-03 13:57:47 -0400

    String Operations

---
+++ GoogleCert/python/string_ops.py
@@ -0,0 +1,17 @@
+# String operations
+new_string = str(123)
+print(type(new_string))
+
+print(len(new_string))
+print("Hello" + "World")
+
+print("Hello".upper())
+print("Hello".lower())
+
+print("HELLO"[1])
+print("HELLO"[1:4])
+
+print("HELLO".index("L"))
+
+my_string = "HELLO"
+my_string[1] = "A"
