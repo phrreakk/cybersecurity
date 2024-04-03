4defe6189f540042ac931e33c61371175e0a7527
Kevin Gangi <kevin_gangi@yahoo.com>
2024-04-03 16:54:29 -0400

    Login check algorithm

--- GoogleCert/python/file.py
+++ GoogleCert/python/file.py
@@ -3,5 +3,18 @@ with open("login_attempts.txt", "r") as file:
     file_text = file.read()
 print(file_text)
 
-words = file_text.split()
-print(words)
+users = file_text.split()
+print(users)
+
+def login_check(login_list, current_user):
+    counter = 0
+    for i in login_list:
+        if i == current_user:
+            counter = counter +1
+    if counter >= 3:
+        return "You have tried to login three or more times.  Your account has been locked."
+    else:
+        return "You can log in!"
+    
+login_response = login_check(users, "hpotter")
+print(login_response)
