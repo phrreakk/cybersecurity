592d8a11693012c22fad0c9a939e4f24e1b2cb1f
Kevin Gangi <kevin_gangi@yahoo.com>
2024-04-03 15:03:42 -0400

    List Operations

---
+++ GoogleCert/python/list_ops.py
@@ -0,0 +1,14 @@
+my_list = ["a", "b", "c", "d", "e"]
+print(my_list[1])
+
+number_list = [1, 2, 3, 4]
+print(my_list + number_list)
+
+my_list[1] = 7
+print(my_list)
+
+my_list.insert(4,"f")
+print(my_list)
+
+my_list.remove("d")
+print(my_list)
